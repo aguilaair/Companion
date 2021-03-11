77b82452deccc5d5abf827b4759aa32ef9235c15
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-03-11 11:37:24 +0100

    Imporved error detection on openInVSCode

--- lib/utils/open_vscode.dart
+++ lib/utils/open_vscode.dart
@@ -5,8 +5,11 @@ import 'package:oktoast/oktoast.dart';
 import 'open_link.dart';
 
 void openInVSCode(String path) {
-  Process.run("code", [path], runInShell: true).onError(
-    (_, __) async {
+  Process.run("code", [path], runInShell: true).then(
+    (value) {
+      if (value.exitCode == 0) {
+        return null;
+      }
       showToast("There was an error opening VSCode,"
           " make sure that it is added to"
           " PATH. Opening via URL...");

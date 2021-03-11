25da4fd127062d1b9e707d2ec1ea68e5464a84f5
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-03-11 11:26:26 +0100

    Remove unused imports

--- lib/components/molecules/project_item.dart
+++ lib/components/molecules/project_item.dart
@@ -1,11 +1,8 @@
-import 'dart:io';
-
 import 'package:Companion/components/atoms/list_tile.dart';
 import 'package:Companion/components/atoms/typography.dart';
 import 'package:Companion/components/molecules/project_version_select.dart';
 
 import 'package:Companion/providers/installed_versions.provider.dart';
-import 'package:Companion/utils/open_link.dart';
 import 'package:Companion/utils/open_vscode.dart';
 
 import 'package:flutter/material.dart';
@@ -13,7 +10,6 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:fvm/fvm.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:oktoast/oktoast.dart';
 
 class ProjectItem extends HookWidget {
   final FlutterProject project;

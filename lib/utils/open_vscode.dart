10976e6a12ca6c641a68698110cbfc0df8df0000
Eduardo <eduardomoreno.eduardo@gmail.com>
2021-03-13 14:23:58 +0100

    Improve VSCode opening logic
    No need to spawn shell on Windows or MacOS now! Linux is next

--- lib/utils/open_vscode.dart
+++ lib/utils/open_vscode.dart
@@ -5,15 +5,79 @@ import 'package:oktoast/oktoast.dart';
 import 'open_link.dart';
 
 void openInVSCode(String path) {
+  if (Platform.isWindows) {
+    _openVSCodeWindows(path);
+  } else if (Platform.isMacOS) {
+    _openVSCodeMacOS(path);
+  } else {
+    multiplatformFallback(path);
+  }
+}
+
+void _openVSCodeViaUrl(String path) {
+  showToast("There was an error opening VSCode,"
+      " make sure that it is installed. Opening via URL...");
+  openLink("vscode://file/$path");
+}
+
+void _openVSCodeWindows(String path) {
+  var pathInfo = Platform.environment["PATH"];
+  var vsCode = pathInfo
+      .split(";")
+      .where(
+        (element) => element.contains("VS Code"),
+      )
+      .toList()
+      .first;
+  Process.run("$vsCode\\code.cmd", [path]).then(
+    (value) {
+      if (value.exitCode != 0) {
+        _openVSCodeViaUrl(path);
+      }
+    },
+  ).onError((error, stackTrace) {
+    _openVSCodeViaUrl(path);
+  });
+}
+
+void _openVSCodeMacOS(String path) async {
+  const pathNormal =
+      "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/";
+  const pathInsider =
+      "/Applications/Visual Studio Code - Insiders.app/Contents/Resources/app/bin/";
+  if (await Directory(pathNormal).exists()) {
+    Process.run("./code", [path], workingDirectory: pathNormal).then(
+      (value) {
+        if (value.exitCode != 0) {
+          _openVSCodeViaUrl(path);
+        }
+      },
+    ).onError((error, stackTrace) {
+      _openVSCodeViaUrl(path);
+    });
+  } else if (await Directory(pathInsider).exists()) {
+    Process.run("./code", [path], workingDirectory: pathInsider).then(
+      (value) {
+        if (value.exitCode != 0) {
+          _openVSCodeViaUrl(path);
+        }
+      },
+    ).onError((error, stackTrace) {
+      _openVSCodeViaUrl(path);
+    });
+  } else {
+    _openVSCodeViaUrl(path);
+  }
+}
+
+void multiplatformFallback(String path) {
   Process.run("code", [path], runInShell: true).then(
     (value) {
-      if (value.exitCode == 0) {
-        return null;
+      if (value.exitCode != 0) {
+        _openVSCodeViaUrl(path);
       }
-      showToast("There was an error opening VSCode,"
-          " make sure that it is added to"
-          " PATH. Opening via URL...");
-      openLink("vscode://file/$path");
     },
-  );
+  ).onError((error, stackTrace) {
+    _openVSCodeViaUrl(path);
+  });
 }
